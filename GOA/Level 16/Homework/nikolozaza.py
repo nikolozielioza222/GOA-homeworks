4ca01caf395f0402dff9fb75e783377d51b2faca
nikolozielioza222 <juzejuzemurinio@gmail.com>
2024-10-28 22:45:02 +0400

    Add files via upload

---
+++ GOA/Level 16/Homework/nikolozaza.py
@@ -0,0 +1,29 @@
+age = 20
+height = 175
+score = 100
+year = 2024
+
+print("Age:", age)
+print("Height:", height)
+print("Score:", score)
+print("Year:", year)
+
+
+
+
+name = "Nikolozi"
+lastname = "elioza"
+mom = "qeti"
+dad = "mama"
+
+print("Name:", name)
+print("last name:", lastname)
+print("moms name:", mom)
+print("dads name:", dad)
+
+
+temp = 98.6
+humidit = 65.4
+
+print("Temperature:", temp)
+print("Humidity:", humidit)
