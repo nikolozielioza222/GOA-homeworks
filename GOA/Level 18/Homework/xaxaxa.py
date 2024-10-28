f217e3b8f6e6e23d5536bf4ee1fb8052a1ab3d9e
nikolozielioza222 <juzejuzemurinio@gmail.com>
2024-10-28 22:49:02 +0400

    commit

---
+++ GOA/Level 18/Homework/xaxaxa.py
@@ -0,0 +1,15 @@
+start = int(input("შეიყვანეთ დიაპაზონის დასაწყისი: "))
+end = int(input("შეიყვანეთ დიაპაზონის დასასრული: "))
+step = int(input("შეიყვანეთ ნაბიჯი (სტეპი): "))
+
+for i in range(start, end , step):
+    print(i)
+
+
+
+for i in range(11):
+    print("nikolozi")
+
+
+for i in range(0, 102, 2):
+    print(i)
---
+++ GOA/Level 21/Homework/make.py
@@ -0,0 +1,25 @@
+animals = []
+
+animals.append("Dog")
+animals.append("Cat")
+animals.append("Bird")
+
+print("animal")
+
+
+numbers = [10, 20, 30]
+
+numbers.insert(1, 15)
+
+print(numbers)
+
+
+
+countries = ['საქართველო', 'საფრანგეთი', 'გერმანია', 'იტალია', 'ესპანეთი']
+
+second_country = countries.pop(1)
+
+last_country = countries.pop()
+
+print("წაშლილი ქვეყნები:", second_country, "და", last_country)
+print("განახლებული სია:", countries)
